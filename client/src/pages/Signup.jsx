d94b1b9ab676052333d1ea47e31141e5157c21d0
AyushGlitch <ayusharyansingh3@gmail.com>
2024-01-21 14:55:03 +0530

    created signup page ui

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,53 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {Button, Label, TextInput} from 'flowbite-react'
 
 export default function Signup() {
   return (
-    <div>Signup</div>
+    <div className=' min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+
+        <div className=' flex-1'>
+          <Link to='/' className='font-bold dark:text-white text-4xl'>
+            <span className=' px-2 py-1 bg-gradient-to-r from-yellow-200 via-orange-400 to-red-600 rounded-lg text-white'>Glitchy</span> Blogs
+          </Link>
+
+          <p className=' text-sm mt-5'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+
+        <div className=' flex-1'>
+          <form className='flex flex-col gap-4'>
+            <div>
+              <Label value='Your Username'/>
+              <TextInput type='text' placeholder='Username' id='username'/>
+            </div>
+
+            <div>
+              <Label value='Your Email'/>
+              <TextInput type='text' placeholder='Email' id='email'/>
+            </div>
+
+            <div>
+              <Label value='Your Password'/>
+              <TextInput type='text' placeholder='Password' id='password'/>
+            </div>
+
+            <Button gradientDuoTone='pinkToOrange' type='submit' outline>
+              Sign Up
+            </Button>
+          </form>
+          
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Have an account ?</span>
+            <Link to='/sign-in' className=' text-blue-500'>
+              Sign In
+            </Link>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
