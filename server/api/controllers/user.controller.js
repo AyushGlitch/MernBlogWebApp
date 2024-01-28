4946afa6be87f3a38feee047d9f18d7ecbfadaa8
AyushGlitch <ayusharyansingh3@gmail.com>
2024-01-28 11:28:57 +0530

    created api route to getUsers

--- server/api/controllers/user.controller.js
+++ server/api/controllers/user.controller.js
@@ -63,3 +63,45 @@ export const signout = (req, res, next) => {
         next(error)
     }
 }
+
+
+export const getUsers = async (req, res, next) => {
+    if(!req.user.isAdmin){
+        return next(errorHandler(401, 'Not an Admin, so not allowed to see all users'))
+    }
+
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0
+        const limit = parseInt(req.query.limit) || 9
+        const sortDirection = req.query.sort === 'asc' ? 1 : -1
+
+        const users = await User.find().sort({createdAt: sortDirection}).skip(startIndex).limit(limit)
+
+        const usersWithoutPassword = users.map( (user) => {
+            const {password, ...others} = user._doc
+            return others
+        })
+
+        const totalUsers = await User.countDocuments()
+        const now = new Date()
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() -1, 
+            now.getDate(),
+        )
+        const lastMonthUsers = await User.countDocuments({
+            createdAt: {$gte: oneMonthAgo}
+        })
+
+        res.status(200).json({
+            success: true,
+            message: 'Users fetched successfully',
+            users: usersWithoutPassword,
+            totalUsers,
+            lastMonthUsers,
+        })
+
+    } catch (error) {
+        next(error)
+    }
+}
