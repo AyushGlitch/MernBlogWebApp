0041bb861116a49088ef6a1ca95e0db86333c025
AyushGlitch <ayusharyansingh3@gmail.com>
2024-01-31 18:54:19 +0530

    added edit comments functionality

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -92,6 +92,13 @@ export default function CommentSection({postId}) {
     }
 
 
+    const handleEdit = async (comment, editedComment) => {
+        setComments(
+            comments.map( (c) => c._id === comment._id ? {...c, content: editedComment} : c)
+        )
+    }
+
+
   return (
     <div className=' max-w-2xl mx-auto w-full p-3'>
         {
@@ -141,7 +148,7 @@ export default function CommentSection({postId}) {
                     
                     {
                         comments.map( comment => (
-                            <Comment key={comment._id} comment={comment} onLike={handleLike}/>
+                            <Comment key={comment._id} comment={comment} onLike={handleLike} onEdit={handleEdit}/>
                         ))
                     }
                 </>
--- server/api/controllers/comment.controller.js
+++ server/api/controllers/comment.controller.js
@@ -68,3 +68,29 @@ export const likeComment = async(req, res, next) => {
         next(error)
     }
 }
+
+
+export const editComment = async(req, res, next) => {
+    try {
+        const comment = await Comment.findById(req.params.commentId)
+        if(!comment){
+            return next(errorHandler(404, 'Comment not found'))
+        }
+
+        if(comment.userId !== req.user.id && !req.user.isAdmin){
+            return next(errorHandler(401, 'Unauthorized, userId not matching or not admin'))
+        }
+
+        const editedComment = await Comment.findByIdAndUpdate(req.params.commentId, {
+            content: req.body.content,
+        }, {new: true})
+
+        res.status(200).json({
+            message: 'Comment edited successfully',
+            comment: editedComment,
+        })
+
+    } catch (error) {
+        next(error)
+    }
+}
