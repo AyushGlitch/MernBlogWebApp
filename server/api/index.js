0bfc993423dc17867d679a8e05e344ece8ed3775
AyushGlitch <ayusharyansingh3@gmail.com>
2024-02-02 17:56:06 +0530

    add pre installation

--- server/api/index.js
+++ server/api/index.js
@@ -5,8 +5,11 @@ import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dbConnect()
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json())
@@ -20,6 +23,12 @@ app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/post', postRoutes)
 app.use('/api/comment', commentRoutes)
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
